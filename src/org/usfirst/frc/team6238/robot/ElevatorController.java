4de5f1155b313bb03edce9fa633b31a82a3d589c
Aditi Khanna <aditikhanna@Aditis-MacBook.local>
2018-10-07 10:51:49 -0700

    elevator code with private instance vars

--- src/org/usfirst/frc/team6238/robot/ElevatorController.java
+++ src/org/usfirst/frc/team6238/robot/ElevatorController.java
@@ -3,12 +3,11 @@ package org.usfirst.frc.team6238.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class ElevatorController implements RobotController {
-
+	private boolean isElevatorButtonPressed;
+	private boolean isElevateDownButtonPressed;
 	@Override
 	public boolean performAction(RobotProperties properties) {
-		boolean isElevatorButtonPressed;
-		boolean isElevateDownButtonPressed;
-		isElevatorButtonPressed = properties.getRightStick().getRawButton(4);
+		isElevatorButtonPressed = properties.getRightStick().getRawButton(5);
 		isElevateDownButtonPressed = properties.getLeftStick().getRawButton(4);
 		//creates a button and set it to port four, returns true if Button is pressed
 		if(isElevatorButtonPressed) {
